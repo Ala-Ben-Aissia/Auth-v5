608958568b121baeb6ada8a4eb16b7483b254305
Ala Ben Aissia <xalabenaissiax@outlook.com>
2025-02-09 17:07:02 +0100

    Register functionality

--- app/actions.ts
+++ app/actions.ts
@@ -1,7 +1,9 @@
 "use server"
 
-import { wait } from "@/lib/utils"
+import { prisma } from "@/lib/client"
+import { getUserByEmail, wait } from "@/lib/utils"
 import { LoginSchema, RegisterSchema } from "@/schemas"
+import { hash } from "bcryptjs"
 import { z } from "zod"
 
 export async function login(data: z.infer<typeof LoginSchema>) {
@@ -16,13 +18,38 @@ export async function login(data: z.infer<typeof LoginSchema>) {
 
 export async function register(data: z.infer<typeof RegisterSchema>) {
   const validatedData = RegisterSchema.safeParse(data)
+
   if (!validatedData.success) {
     return { success: false, message: "Invalid input data" }
   }
-  const { password, confirmPassword } = validatedData.data
+
+  const { password, confirmPassword, name, email } = validatedData.data
+
   if (password !== confirmPassword) {
     return { success: false, message: "Passwords do not match" }
   }
-  await wait(1000)
-  return { success: true, message: "Register successful" }
+
+  const hashedPassword = await hash(password, 10)
+  const ExisitingUser = await getUserByEmail(email)
+
+  if (ExisitingUser) {
+    return { success: false, message: "User already exists" }
+  }
+
+  try {
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+    return { success: true, message: "Register successful" }
+  } catch (error) {
+    console.error("Failed to create user:", error)
+    return {
+      success: false,
+      message: "Failed to create user account. Please try again later.",
+    }
+  }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { prisma } from "@/lib/client"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -7,4 +8,12 @@ export function cn(...inputs: ClassValue[]) {
 
 export function wait(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export function getUserById(id: string) {
+  return prisma.user.findUnique({ where: { id } })
+}
+
+export function getUserByEmail(email: string) {
+  return prisma.user.findUnique({ where: { email } })
 }
